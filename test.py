9e51e0cf521b87efdae95d78a1c86784d1a3f227
Shubham <shubhamp.singh@yahoo.com>
2018-10-04 18:40:54 +0530

    My Twitter data analysis, first commit

---
+++ test.py
@@ -0,0 +1,6 @@
+import sentiment_mod as s
+
+
+print(s.sentiment("Strong performances and fine moments that weave into a convincing story."))
+print(s.sentiment("Predictable but fun spoof of a spy film"))
+print(s.sentiment("The animation is delightful without being over the top. The script is pretty straightforward and will keep kids entertained although it may not find many adult takers."))
---
+++ twitter_fetch.py
@@ -0,0 +1,37 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+import sentiment_mod as s
+
+
+#consumer key, consumer secret, access token, access secret.
+ckey="4VAiBAB6w1zkRmZOv491JpOhv"
+csecret="24J1JrSoXLQj0j468IPAuKKseentO94evrCtmqlHGVxc1CDIJZ"
+atoken="1027182879401435137-2IyYON2AtzdplfPmZ3QdG8l5lqRojY"
+asecret="dlwhNXVnACOs4oRyeOlcs9ERRE2cKBCekLVUmKYLtkxHS"
+
+class listener(StreamListener):
+
+    def on_data(self, data):
+        all_data=json.loads(data)
+        tweet= ascii(all_data["text"])
+        sentiment_value, confidence = s.sentiment(tweet)
+        print(tweet, sentiment_value, confidence)
+
+        if confidence*100 >= 80:
+            output = open("twitter-out.txt","a")
+            output.write(sentiment_value)
+            output.write('\n')
+            output.close()
+        return(True)
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["education"])
+		
